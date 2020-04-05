1578e005108388d27ad5efe1f7b97679a571c2a9
Electrotelco <60045362+electrotelco@users.noreply.github.com>
2020-04-05 22:58:18 +0200

    Add files via upload

---
+++ tumbarse/config.lua
@@ -0,0 +1,18 @@
+Config              = {}
+Config.MaxDistance = 1.5
+
+Config.BedList = {
+
+	{
+		text = 'Pulsa [E] para tumbarte en la cama',
+		heading = -20.0, 
+		objCoords  = {x = 348.72, y = -579.45, z = 43.11},---Sala Rayos X
+	},
+
+	{
+		text = 'Pulsa [E] para tumbarte en la cama',
+		heading = -20.0, 
+		objCoords  = {x = 337.07, y = -575.16, z = 43.11, a = 50.00},---Sala RM
+	}
+
+}
